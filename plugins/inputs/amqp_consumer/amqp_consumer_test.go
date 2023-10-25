a271ae4c91fc6c0e0a5656e9a35b39494b57c282
k <wmxhw@outlook.com>
2023-10-25 17:39:40 +0800

    feat(inputs.amqp_consumer): AMQP input add check queue for the presence of consumers

--- plugins/inputs/amqp_consumer/amqp_consumer_test.go
+++ plugins/inputs/amqp_consumer/amqp_consumer_test.go
@@ -1,15 +1,18 @@
 package amqp_consumer
 
 import (
-	"testing"
-
 	"github.com/rabbitmq/amqp091-go"
 	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+	"time"
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/plugins/parsers/influx"
 	"github.com/influxdata/telegraf/testutil"
+
+	_ "net/http/pprof"
 )
 
 func TestAutoEncoding(t *testing.T) {
@@ -49,4 +52,95 @@ func TestAutoEncoding(t *testing.T) {
 	err = a.onMessage(acc, d)
 	require.NoError(t, err)
 	acc.AssertContainsFields(t, "measurementName2", map[string]interface{}{"fieldKey": "identity"})
+}
+
+func TestQueueConsume(t *testing.T) {
+	// pprof
+	go http.ListenAndServe(":80", nil)
+
+	rmq := AMQPConsumer{
+		Brokers: []string{
+			"amqp://username:password@127.0.0.1:5672",
+		},
+		Exchange:     "test_exchange",
+		ExchangeType: "direct",
+		Queue:        "test_queue",
+		//QueuePassive:              true,
+		QueueConsumeCheck:         true,
+		QueueConsumeCheckInterval: time.Second * 10,
+		BindingKey:                "#",
+		Log: testutil.Logger{
+			Name: "amqp_consumer",
+		},
+	}
+
+	if err := rmq.Start(new(ac)); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("waiting...")
+
+	select {}
+	// delete the "test_queue" queue to test checkQueueConsume.
+	// test result:
+	// 		Errorf Error inspecting queue: Exception (404) Reason: "NOT_FOUND - no queue 'test_queue' in vhost '/'"
+	// 		Errorf Error inspect queue test_queue: no consumers
+	// 		panic: QueueConsumeCheckFailCallback test_queue
+}
+
+type ac struct{}
+
+func (a ac) AddFields(_ string, _ map[string]interface{}, _ map[string]string, _ ...time.Time) {}
+
+func (a ac) AddGauge(_ string, _ map[string]interface{}, _ map[string]string, _ ...time.Time) {}
+
+func (a ac) AddCounter(_ string, _ map[string]interface{}, _ map[string]string, _ ...time.Time) {}
+
+func (a ac) AddSummary(_ string, _ map[string]interface{}, _ map[string]string, _ ...time.Time) {}
+
+func (a ac) AddHistogram(_ string, _ map[string]interface{}, _ map[string]string, _ ...time.Time) {}
+
+func (a ac) AddMetric(_ telegraf.Metric) {}
+
+func (a ac) SetPrecision(_ time.Duration) {}
+
+func (a ac) AddError(_ error) {}
+
+func (a ac) WithTracking(_ int) telegraf.TrackingAccumulator {
+	return tkAc{}
+}
+
+type tkAc struct{}
+
+func (t2 tkAc) AddFields(_ string, _ map[string]interface{}, _ map[string]string, _ ...time.Time) {}
+
+func (t2 tkAc) AddGauge(_ string, _ map[string]interface{}, _ map[string]string, _ ...time.Time) {}
+
+func (t2 tkAc) AddCounter(_ string, _ map[string]interface{}, _ map[string]string, _ ...time.Time) {}
+
+func (t2 tkAc) AddSummary(_ string, _ map[string]interface{}, _ map[string]string, _ ...time.Time) {}
+
+func (t2 tkAc) AddHistogram(_ string, _ map[string]interface{}, _ map[string]string, _ ...time.Time) {
+}
+
+func (t2 tkAc) AddMetric(_ telegraf.Metric) {}
+
+func (t2 tkAc) SetPrecision(_ time.Duration) {}
+
+func (t2 tkAc) AddError(_ error) {}
+
+func (t2 tkAc) WithTracking(_ int) telegraf.TrackingAccumulator {
+	return nil
+}
+
+func (t2 tkAc) AddTrackingMetric(_ telegraf.Metric) telegraf.TrackingID {
+	return 0
+}
+
+func (t2 tkAc) AddTrackingMetricGroup(_ []telegraf.Metric) telegraf.TrackingID {
+	return 0
+}
+
+func (t2 tkAc) Delivered() <-chan telegraf.DeliveryInfo {
+	return nil
 }
